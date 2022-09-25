3c83710bd60ce9e741d9699cfeba162ce97d7272
Carlos Soto <soto.simas@gmail.com>
2022-09-25 11:36:19 +0100

    selected gif component

---
+++ src/components/gif.jsx
@@ -0,0 +1,11 @@
+import React, { Component} from 'react';
+
+class Gif extends Component {
+  render() {
+    return (
+      <img src='https://media4.giphy.com/media/9nZKqcn4phkT30zeN8/giphy.gif' className='gif'/>
+      );
+  }
+}
+
+export default Gif;
