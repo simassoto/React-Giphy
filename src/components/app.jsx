bf2c9183a90df867cd9f9b42692c469bd5147e41
Carlos Soto <soto.simas@gmail.com>
2022-09-25 19:25:42 +0100

    giflist

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,20 +2,27 @@ import React, { Component } from "react";
 
 import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
 
 class App extends Component {
   render() {
+    const gifs = [
+      {id:"9nZKqcn4phkT30zeN8" }
+
+    ];
     return (
       <div>
         <div className="left-scene">
           <SearchBar/>
           <div className="selected-gif">
-          <Gif/>
+          <Gif id="9nZKqcn4phkT30zeN8"/>
           </div>
         </div>
-        <div className="right-scene"></div>
+        <div className="right-scene">
+          <GiftList gifs={gifs}/>
+        </div>
       </div>
-    )
+    );
   }
 }
 
--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -2,8 +2,9 @@ import React, { Component} from 'react';
 
 class Gif extends Component {
   render() {
+    const src = `https://media4.giphy.com/media/${this.props.id}/giphy.gif`;
     return (
-      <img src='https://media4.giphy.com/media/9nZKqcn4phkT30zeN8/giphy.gif' className='gif'/>
+      <img src= {src} className='gif'/>
       );
   }
 }
